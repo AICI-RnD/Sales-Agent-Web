c8df80bb154184d05aafee835ce7e03a22945c94
Anh Đạt Ghét Sự Zã Zối <tranthanhdat2301@gmail.com>
2025-11-03 16:37:49 +0700

    f1

--- src/App.js
+++ src/App.js
@@ -1,44 +1,21 @@
+// 
+
 // src/App.js
 import React from 'react';
 import HomePage from './pages/HomePage/HomePage';
-import LoginPage from './pages/LoginPage/LoginPage';
 import { ViewProvider } from './context/ActiveAgentContext'; // Đổi tên file này thành ViewContext.js cho rõ ràng
 // import CustomCursor from './components/CustomCursor/CustomCursor';
-import { AuthProvider, useAuth } from './context/AuthContext';
 import './index.css';
-
-// function AppContent() {
-//   const { isAuthenticated } = useAuth();
-
-//   // Nếu đã đăng nhập, hiển thị HomePage (đã bao gồm Header, Sidebar...)
-//   // Nếu chưa, hiển thị LoginPage
-//   return isAuthenticated ? (
-//     <ViewProvider>
-//       <HomePage />
-//     </ViewProvider>
-//   ) : (
-//     <LoginPage />
-//   );
-// }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <AuthProvider> {/* Bọc toàn bộ App bằng AuthProvider */}
-//         {/* <CustomCursor /> */}
-//         <AppContent /> {/* Hiển thị nội dung chính */}
-//       </AuthProvider>
-//     </div>
-//   );
-// }
 
 function App() {
   return (
     <div className="App">
-        {/* <CustomCursor /> */}
-        <AppContent /> {/* Hiển thị nội dung chính */}
+      {/* <CustomCursor /> */}
+      <ViewProvider>
+        <HomePage />
+      </ViewProvider>
     </div>
   );
 }
 
-export default App
+export default App;

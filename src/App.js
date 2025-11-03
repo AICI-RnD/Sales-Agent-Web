e5ed99b6cb1664f9bc3731dbf7ea8d9f5cb1c527
Anh Đạt Ghét Sự Zã Zối <tranthanhdat2301@gmail.com>
2025-11-03 16:33:35 +0700

    f1

--- src/App.js
+++ src/App.js
@@ -7,27 +7,36 @@ import { ViewProvider } from './context/ActiveAgentContext'; // Đổi tên file
 import { AuthProvider, useAuth } from './context/AuthContext';
 import './index.css';
 
-function AppContent() {
-  const { isAuthenticated } = useAuth();
+// function AppContent() {
+//   const { isAuthenticated } = useAuth();
 
-  // Nếu đã đăng nhập, hiển thị HomePage (đã bao gồm Header, Sidebar...)
-  // Nếu chưa, hiển thị LoginPage
-  return isAuthenticated ? (
-    <ViewProvider>
-      <HomePage />
-    </ViewProvider>
-  ) : (
-    <LoginPage />
-  );
-}
+//   // Nếu đã đăng nhập, hiển thị HomePage (đã bao gồm Header, Sidebar...)
+//   // Nếu chưa, hiển thị LoginPage
+//   return isAuthenticated ? (
+//     <ViewProvider>
+//       <HomePage />
+//     </ViewProvider>
+//   ) : (
+//     <LoginPage />
+//   );
+// }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <AuthProvider> {/* Bọc toàn bộ App bằng AuthProvider */}
+//         {/* <CustomCursor /> */}
+//         <AppContent /> {/* Hiển thị nội dung chính */}
+//       </AuthProvider>
+//     </div>
+//   );
+// }
 
 function App() {
   return (
     <div className="App">
-      <AuthProvider> {/* Bọc toàn bộ App bằng AuthProvider */}
         {/* <CustomCursor /> */}
         <AppContent /> {/* Hiển thị nội dung chính */}
-      </AuthProvider>
     </div>
   );
 }
